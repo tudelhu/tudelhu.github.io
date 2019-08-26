39c875800476b3752b411232eb53fd2dd35da9bb
Leonhard Rabel <rabel@leitwert.net>
2019-08-26 17:36:15 +0200

    test

--- sample-tool/index.js
+++ sample-tool/index.js
@@ -15,8 +15,132 @@
  */
 'use strict';
 
+var Marzipano = window.Marzipano;
+
+// Custom control method to alter the view according to the device orientation.
+function DeviceOrientationControlMethod() {
+  this._dynamics = {
+    yaw: new Marzipano.Dynamics(),
+    pitch: new Marzipano.Dynamics()
+  };
+
+  this._deviceOrientationHandler = this._handleData.bind(this);
+
+  if (window.DeviceOrientationEvent) {
+    window.addEventListener('deviceorientation', this._deviceOrientationHandler);
+  } else {
+    alert('DeviceOrientationEvent not defined');
+  }
+
+  this._previous = {};
+  this._current = {};
+  this._tmp = {};
+
+  this._getPitchCallbacks = [];
+}
+
+Marzipano.dependencies.eventEmitter(DeviceOrientationControlMethod);
+
+DeviceOrientationControlMethod.prototype.destroy = function() {
+  this._dynamics = null;
+  if (window.DeviceOrientationEvent) {
+    window.removeEventListener('deviceorientation', this._deviceOrientationHandler);
+  }
+  this._deviceOrientationHandler = null;
+  this._previous = null;
+  this._current = null;
+  this._tmp = null;
+  this._getPitchCallbacks = null;
+};
+
+
+DeviceOrientationControlMethod.prototype.getPitch = function(cb) {
+  this._getPitchCallbacks.push(cb);
+};
+
+
+DeviceOrientationControlMethod.prototype._handleData = function(data) {
+  var previous = this._previous,
+      current = this._current,
+      tmp = this._tmp;
+
+  tmp.yaw = Marzipano.util.degToRad(data.alpha);
+  tmp.pitch = Marzipano.util.degToRad(data.beta);
+  tmp.roll = Marzipano.util.degToRad(data.gamma);
+
+  rotateEuler(tmp, current);
+
+  // Report current pitch value.
+  this._getPitchCallbacks.forEach(function(callback) {
+    callback(null, current.pitch);
+  });
+  this._getPitchCallbacks.length = 0;
+
+  // Emit control offsets.
+  if (previous.yaw != null && previous.pitch != null && previous.roll != null) {
+    this._dynamics.yaw.offset = -(current.yaw - previous.yaw);
+    this._dynamics.pitch.offset = (current.pitch - previous.pitch);
+
+    this.emit('parameterDynamics', 'yaw', this._dynamics.yaw);
+    this.emit('parameterDynamics', 'pitch', this._dynamics.pitch);
+  }
+
+  previous.yaw = current.yaw;
+  previous.pitch = current.pitch;
+  previous.roll = current.roll;
+};
+
+// Taken from krpano's gyro plugin by Aldo Hoeben:
+// https://github.com/fieldOfView/krpano_fovplugins/tree/master/gyro/
+// For the math, see references:
+// http://www.euclideanspace.com/maths/geometry/rotations/conversions/eulerToMatrix/index.htm
+// http://www.euclideanspace.com/maths/geometry/rotations/conversions/matrixToEuler/index.htm
+function rotateEuler(euler, result) {
+  var heading, bank, attitude,
+    ch = Math.cos(euler.yaw),
+    sh = Math.sin(euler.yaw),
+    ca = Math.cos(euler.pitch),
+    sa = Math.sin(euler.pitch),
+    cb = Math.cos(euler.roll),
+    sb = Math.sin(euler.roll),
+
+    matrix = [
+      sh*sb - ch*sa*cb,   -ch*ca,    ch*sa*sb + sh*cb,
+      ca*cb,              -sa,      -ca*sb,
+      sh*sa*cb + ch*sb,    sh*ca,   -sh*sa*sb + ch*cb
+    ]; // Includes 90-degree rotation around z axis
+
+  /* [m00 m01 m02] 0 1 2
+   * [m10 m11 m12] 3 4 5
+   * [m20 m21 m22] 6 7 8 */
+
+  if (matrix[3] > 0.9999)
+  {
+    // Deal with singularity at north pole
+    heading = Math.atan2(matrix[2],matrix[8]);
+    attitude = Math.PI/2;
+    bank = 0;
+  }
+  else if (matrix[3] < -0.9999)
+  {
+    // Deal with singularity at south pole
+    heading = Math.atan2(matrix[2],matrix[8]);
+    attitude = -Math.PI/2;
+    bank = 0;
+  }
+  else
+  {
+    heading = Math.atan2(-matrix[6],matrix[0]);
+    bank = Math.atan2(-matrix[5],matrix[4]);
+    attitude = Math.asin(matrix[3]);
+  }
+
+  result.yaw = heading;
+  result.pitch = attitude;
+  result.roll = bank;
+}
+
 (function() {
-  var Marzipano = window.Marzipano;
   var bowser = window.bowser;
   var screenfull = window.screenfull;
   var data = window.APP_DATA;
@@ -177,6 +301,8 @@
   controls.registerMethod('rightElement', new Marzipano.ElementPressControlMethod(viewRightElement,  'x',  velocity, friction), true);
   controls.registerMethod('inElement',    new Marzipano.ElementPressControlMethod(viewInElement,  'zoom', -velocity, friction), true);
   controls.registerMethod('outElement',   new Marzipano.ElementPressControlMethod(viewOutElement, 'zoom',  velocity, friction), true);
+  var deviceOrientationControlMethod = new DeviceOrientationControlMethod(); // Neu
+  controls.registerMethod('deviceOrientation', deviceOrientationControlMethod); // Neu
 
   function sanitize(s) {
     return s.replace('&', '&amp;').replace('<', '&lt;').replace('>', '&gt;');
@@ -389,4 +515,50 @@
   // Display the initial scene.
   switchScene(scenes[0]);
 
+  // Neu ab hier vvvvv
+  // Set up control for enabling/disabling device orientation.
+
+  var enabled = false;
+
+  var toggleElement = document.createElement('A');
+  toggleElement.setAttribute('id', 'toggleDeviceOrientation');
+  toggleElement.setAttribute('href', 'javascript:void(0)');
+
+  var toggleElementImgEnable = document.createElement('IMG');
+  toggleElementImgEnable.setAttribute('class', 'enable');
+  toggleElementImgEnable.setAttribute('src', 'img/enable.png');
+
+  var toggleElementImgDisable = document.createElement('IMG');
+  toggleElementImgDisable.setAttribute('class', 'disable');
+  toggleElementImgDisable.setAttribute('src', 'img/disable.png');
+
+  toggleElement.appendChild(toggleElementImgEnable);
+  toggleElement.appendChild(toggleElementImgDisable);
+
+  document.body.appendChild(toggleElement);
+
+  function enable() {
+    deviceOrientationControlMethod.getPitch(function(err, pitch) {
+      if (!err) {
+        scenes[0].view.setPitch(pitch);
+      }
+    });
+    controls.enableMethod('deviceOrientation');
+    enabled = true;
+    toggleElement.className = 'enabled';
+  }
+
+  function disable() {
+    controls.disableMethod('deviceOrientation');
+    enabled = false;
+    toggleElement.className = '';
+  }
+
+  function toggle() {
+    if (enabled) {
+      disable();
+    } else {
+      enable();
+    }
+  }
 })();
